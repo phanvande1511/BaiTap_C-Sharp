40161ebbf7e8f8f7205da7177c17888f98923d49
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-16 09:22:57 +0700

    BaiTap

---
+++ BaiTap2/BaiTap2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BaiTap2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap ten cua ban: ");
+            string ten = Console.ReadLine();
+            Console.WriteLine("Hello " + ten);
+        }
+    }
+}
---
+++ BaiTap3/BaiTap3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BaiTap3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so A: ");
+            float soA = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Nhap vao so B: ");
+            float soB = Convert.ToSingle(Console.ReadLine());
+            float soC = 0;
+            soC = soA + soB;
+            Console.Write("Tong cua " + soA + " va " + soB + " la " +soC);
+            Console.ReadLine();
+        }
+    }
+}
