84d571cc05985805ce897186e9c87720a3707e54
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-16 09:40:43 +0700

    Baitap

---
+++ Baitap4/Baitap4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Baitap4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Cach giai pt bac nhat co dang: ax + b = 0");
+            Console.Write("Nhap vao so a: ");
+            float soa = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Nhap vao so b: ");
+            float sob = Convert.ToSingle(Console.ReadLine());
+            if (soa == 0 && sob == 0)
+            {
+                Console.WriteLine("Pt co vo so nghiem.");
+            }
+            if (soa == 0 && sob != 0)
+            {
+                Console.WriteLine("Pt vo nghiem.");
+            }
+            if (soa != 0)
+            {
+                Console.WriteLine("Pt co 1 nghiem duy nhat: x = " + ((-sob) / soa));
+            }
+        }
+    }
+}
