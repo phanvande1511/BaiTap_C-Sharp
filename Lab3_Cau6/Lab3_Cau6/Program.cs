d6cee5dc5631bc209e35b46e3b3e8c400f24b4a0
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-18 15:54:10 +0700

    BaiTap

---
+++ Lab3_Cau6/Lab3_Cau6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab3_Cau6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap vao 1 ki tu Alphabet: ");
+            char kiTu = Convert.ToChar(Console.ReadLine());
+            byte ascii = (byte)kiTu;
+            
+            if (ascii <= 90 && ascii >= 65 || ascii >= 97 && ascii <= 122)
+            {
+                Console.WriteLine("\nKi tu nay thuoc bang Alphabet");
+                if (ascii == 65 || ascii == 97 || ascii == 69 || ascii == 101 || ascii == 79 || ascii == 111 || ascii == 73 || ascii == 105 || ascii == 85 || ascii == 117)
+                {
+                    Console.WriteLine("\nDay la nguyen am");
+                } else
+                {
+                    Console.WriteLine("\nDay la phu am");
+                }
+            } else
+            {
+                Console.WriteLine("\nKi tu nay khong thuoc bang Alphabet");
+            }
+
+        }
+    }
+}
