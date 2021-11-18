c240581055a054691dacdcaca934b39e4123f1ce
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-18 10:18:55 +0700

    BaiTap

---
+++ Lab3_Cau4/Lab3_Cau4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab3_Cau4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap canh a: ");
+            int a = System.Int32.Parse(Console.ReadLine());
+            Console.Write("Nhap canh b: ");
+            int b = System.Int32.Parse(Console.ReadLine());
+            Console.Write("Nhap canh c: ");
+            int c = System.Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------");
+
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                Console.WriteLine("Day khong phai la hinh tam giac");
+            } else if (Math.Abs(a - b) < c && Math.Abs(a - c) < b && Math.Abs(c - b) < a)
+            {
+                Console.WriteLine("Day la hinh tam giac");
+            } else
+            {
+                Console.WriteLine("Day khong phai la hinh tam giac");
+            }
+        }
+    }
+}
