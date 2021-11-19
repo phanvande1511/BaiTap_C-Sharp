d6b62666adda13cd7a60448f86add06ab2608b4d
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-19 14:51:52 +0700

    BaiTap

---
+++ BaiTap7/BaiTap7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BaiTap7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\tMENU");
+            Console.WriteLine("===================");
+            Console.Write("Nhap vao so nguyen a: ");
+            float soA = System.Single.Parse(Console.ReadLine());
+            Console.Write("Nhap vao so nguyen b: ");
+            float soB = System.Single.Parse(Console.ReadLine());
+
+            float tong = soA + soB;
+            float hieu = soA - soB;
+            float tich = soA * soB;
+            float thuong = soA / soB;
+
+            Console.WriteLine("\nTong: {0} + {1} = {2}", soA, soB, tong);
+            Console.WriteLine("Hieu: {0} - {1} = {2}", soA, soB, hieu);
+            Console.WriteLine("Tich: {0} * {1} = {2}", soA, soB, tich);
+            Console.WriteLine("Thuong: {0} : {1} = {2}", soA, soB, thuong);
+            Console.WriteLine("===================");
+            Console.WriteLine("Ket thuc");
+        }
+    }
+}
