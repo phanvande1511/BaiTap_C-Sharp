a2317ff92f17b7bc27b5213d9d7685fbedafd2d2
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-19 21:00:54 +0700

    BaiTap

---
+++ BaiTap9/BaiTap9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BaiTap9
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Tong cua 10 so chan dau tien: {0}", sum);
+        }
+    }
+}
