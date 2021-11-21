ab7ba76d159ab0d3202404d114041977dd94c4c8
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-21 17:09:26 +0700

    BaiTap

---
+++ Lab4_Cau5/Lab4_Cau5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab4_Cau5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap vao 1 so nguyen bat ki: ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int temp = 0;
+            while (n > 0)
+            {
+                temp = n % 10;
+                n = n / 10;
+                sum += temp;
+            }
+            Console.WriteLine("Tong cua so nguyen kia la : {0}", sum);
+        }
+    }
+}
