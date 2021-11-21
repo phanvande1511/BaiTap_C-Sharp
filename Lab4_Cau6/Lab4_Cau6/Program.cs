be7e40eed484a4e422480c8ab9fbac1a8c9c5d03
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-21 18:57:01 +0700

    BaiTap

---
+++ Lab4_Cau6/Lab4_Cau6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab4_Cau6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap so Fabonacy ma ban can: ");
+            int n = int.Parse(Console.ReadLine());
+            int f0 = 0;
+            int f1 = 1;
+            int fn;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i <= 1)
+                {
+                    fn = i;
+                } else
+                {
+                    fn = f1 + f0;
+                    f0 = f1;
+                    f1 = fn;
+                }
+                Console.WriteLine("So Fabocany thu {0} la: {1}", i, fn);
+            }
+        }
+    }
+}
---
+++ Lab4_Cau7/Lab4_Cau7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab4_Cau7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap vao 1 so bat ki: ");
+
+        }
+    }
+}
