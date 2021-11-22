a608238a8b44b0e2b10db87d86588b76ad98cc99
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-22 21:13:42 +0700

    BaiTap

---
+++ BaiTap19/BaiTap19/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BaiTap19
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int min = 0;
+            Console.Write("Enter number A: ");
+            int numberA = int.Parse(Console.ReadLine());
+            Console.Write("Enter number B: ");
+            int numberB = int.Parse(Console.ReadLine());
+
+            if (numberA > numberB)
+            {
+                min = numberB;
+            }
+            if (numberB > numberA)
+            {
+                min = numberA;
+            }
+            if (numberA == numberB)
+            {
+                min = numberB;
+            }
+            
+            if (numberA > 0 && numberB > 0)
+            {
+                for (int i = min; i > 0; i--)
+                {
+                    if (numberA % i == 0 && numberB % i ==0)
+                    {
+                        Console.WriteLine("Greatest Common Divisor: {0}", i);
+                        break;
+                    }
+                }
+                
+            } else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
