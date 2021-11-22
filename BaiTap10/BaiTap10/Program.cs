499c31392d9e42c05bcd27511a952d37c7ffbf94
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-22 13:33:08 +0700

    BaiTap

---
+++ BaiTap10/BaiTap10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BaiTap10
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Cac so le tu 1 - 99 la: ");
+            Console.ReadKey();
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("\t {0} \n", i);
+                }
+            }
+        }
+    }
+}
