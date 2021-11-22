4d5b883b197f298ef3481c17a43612971d880d77
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-22 13:46:22 +0700

    BaiTap

---
+++ BaiTap11/BaiTap11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BaiTap11
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number > 0 && number <= 100)
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    if (i % number == 0)
+                    {
+                        Console.WriteLine("Multiples: {0}", i);
+                    }
+                }
+            } else
+            {
+                Console.WriteLine("Error!!!");
+            }
+        }
+    }
+}
