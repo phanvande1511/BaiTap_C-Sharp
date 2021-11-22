5020645032c35c15fdea095c691959cd2560c700
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-22 13:57:44 +0700

    BaiTap

---
+++ BaiTap16/BaiTap16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BaiTap16
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number >= 0)
+            {
+                if (number == 0)
+                {
+                    Console.WriteLine("This is zero");
+                } else if (number % 2 == 0)
+                {
+                    Console.WriteLine("This is even number");
+                } else
+                {
+                    Console.WriteLine("This is odd number");
+                }
+            } else
+            {
+                Console.WriteLine("Error"); 
+            }
+        }
+    }
+}
