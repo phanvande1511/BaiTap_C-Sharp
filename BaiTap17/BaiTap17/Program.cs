dcbe576fffddb63c5f421bcd776f59eb4ee00c06
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-11-22 17:34:56 +0700

    BaiTap

---
+++ BaiTap17/BaiTap17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BaiTap17
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Multiples of 3: ");
+            for (int i = 3; i <= 300; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ BaiTap18/BaiTap18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BaiTap18
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            int temp = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    temp++;
+                }
+            }
+            if (temp == 2)
+            {
+                Console.WriteLine("That Is Prime Number");
+            } else
+            {
+                Console.WriteLine("That Isn't Prime Number");
+            }
+        }
+    }
+}
