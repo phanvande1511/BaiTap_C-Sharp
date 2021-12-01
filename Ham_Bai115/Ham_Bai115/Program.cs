12ea203bde63a6d682f63e4434b76c6f884dcb6e
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-01 16:28:51 +0700

    BaiTap

---
+++ Ham_Bai115/Ham_Bai115/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ham_Bai115
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Information();
+            Console.WriteLine("Diem TB cua mon Toan va mon Van la: {0}", Average());
+        }
+
+        static void Information()
+        {
+            Console.Write("Nhap ho ten cua ban: ");
+            string name = Console.ReadLine();
+        }
+
+        static float Average()
+        {
+            Console.Write("Nhap diem toan cua ban: ");
+            float dToan = float.Parse(Console.ReadLine());
+            Console.Write("Nhap diem van cua ban: ");
+            float dVan = float.Parse(Console.ReadLine());
+            float average = (dToan + dVan) / 2;
+            return average;
+        }
+    }
+}
