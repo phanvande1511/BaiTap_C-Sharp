a82d5ac4d598221d967692549570bcecedea4acf
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-02 10:20:30 +0700

    BaiTap

--- Ham_Bai115/Ham_Bai115/Program.cs
+++ Ham_Bai115/Ham_Bai115/Program.cs
@@ -6,24 +6,19 @@ namespace Ham_Bai115
     {
         public static void Main(string[] args)
         {
-            Information();
-            Console.WriteLine("Diem TB cua mon Toan va mon Van la: {0}", Average());
-        }
-
-        static void Information()
-        {
             Console.Write("Nhap ho ten cua ban: ");
             string name = Console.ReadLine();
-        }
-
-        static float Average()
-        {
             Console.Write("Nhap diem toan cua ban: ");
             float dToan = float.Parse(Console.ReadLine());
             Console.Write("Nhap diem van cua ban: ");
             float dVan = float.Parse(Console.ReadLine());
+            Console.WriteLine("Diem TB cua mon Toan va mon Van la: {0}", Average(dToan, dVan));
+        }
+
+        static float Average(float dToan, float dVan)
+        {
             float average = (dToan + dVan) / 2;
             return average;
-        }
+        }  
     }
 }
---
+++ Ham_Bai116/Ham_Bai116/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ham_Bai116
+{
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao 1 so nguyen N: ");
+            int number = int.Parse(Console.ReadLine());
+            SumNumber(number);
+            
+        }
+
+        static int SumNumber(int number)
+        {
+            int temp = 0;
+            if (number > 0)
+            {
+                for (int i = 0; i <= number; i++)
+                {
+                    temp += i;
+                }
+                Console.WriteLine("Tong tu 1 den {0} la: {1}", number, temp);
+            }
+            else
+            {
+                Console.WriteLine("Nhap loi");
+            }
+            return temp;
+        }
+    }
+}
