47e074d5806e4f55d07e0f0f1b56967b29d9efcc
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-04 10:42:07 +0700

    BaiTap

---
+++ Lab6_Bai7/Lab6_Bai7/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Lab6_Bai7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Menu();
+            int number = 0;
+            Choose(number);
+        }
+        static void Menu()
+        {
+            Console.WriteLine("+-----------------------------------------------+");
+            Console.WriteLine("|\t\t\tMENU\t\t\t|");
+            Console.WriteLine("+-----------------------------------------------+");
+            Console.WriteLine("|1. Menu 1\t\t\t\t\t|");
+            Console.WriteLine("|2. Menu 2\t\t\t\t\t|");
+            Console.WriteLine("|3. Menu 3\t\t\t\t\t|");
+            Console.WriteLine("|4. Menu 4\t\t\t\t\t|");
+            Console.WriteLine("|5. Exit\t\t\t\t\t|");
+            Console.WriteLine("+-----------------------------------------------+");
+        }
+
+        static void Choose(int number)
+        {
+            do
+            {
+                Console.Write("Chon 1 so tu 1 - 5: ");
+                number = int.Parse(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("Ban chon menu 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Ban chon menu 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("Ban chon menu 3");
+                        break;
+                    case 4:
+                        Console.WriteLine("Ban chon menu 4");
+                        break;
+                    case 5:
+                        Console.WriteLine("Ban chon thoat khoi day");
+                        break;
+                }
+            } while (number != 1 && number != 2 && number != 3 && number != 4 && number != 5);
+        }
+    }
+}
---
+++ Mang_Bai122/Mang_Bai122/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Mang_Bai122
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap so luong chu so: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] number = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Nhap so[{0}]: ", i);
+                number[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("So lon nhat trong mang la: {0}", Max(number);
+        }
+
+        static int Max(int[] number, int size)
+        {
+            int max = number[0];
+            for (int j = 0; j < size; j++)
+            {
+                if (max < number[j])
+                {
+                    max = number[j];
+                }
+            }
+            return max;
+        }
+    } 
+}
