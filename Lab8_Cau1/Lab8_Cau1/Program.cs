8a9e9446f734ec296d146514f3b7807a8772af3f
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-05 07:31:43 +0700

    BaiTap

---
+++ Lab8_Cau1/Lab8_Cau1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab8_Cau1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhap vao 1 chuoi bat ki: ");
+            string chuoi = Console.ReadLine();
+            Console.Write("Nhap vao 1 ki tu: ");
+            string temp = Console.ReadLine();
+            int dem = 0;
+
+            for (int i = 0; i < chuoi.Length; i++)
+            {
+                if (chuoi[i] == temp[0])
+                {
+                    dem++;
+                }
+            }
+
+            Console.WriteLine("So luong ki tu '{0}' trong chuoi la: {1}", temp[0], dem);
+        }
+    }
+}
