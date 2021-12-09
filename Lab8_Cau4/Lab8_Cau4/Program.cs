158498549e9375d7205d4b91e24519daa2f11ec9
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-09 16:04:21 +0700

    BaiTap

---
+++ Lab8_Cau4/Lab8_Cau4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab8_Cau4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string chuoi;
+            string temp = "";
+
+            Console.Write("Nhap chuoi: ");
+            chuoi = Console.ReadLine();
+
+            chuoi = chuoi.Trim();
+
+            while (chuoi.IndexOf("  ") != -1)// vi tri xuat hien 
+            {
+                chuoi = chuoi.Replace("  ", " ");// thay the
+            }
+
+            string[] SubName = chuoi.Split(' ');//tach chuoi thanh mang
+
+            for (int i = 0; i < SubName.Length; i++)
+            {
+                string FirstChar = SubName[i].Substring(0, 1);
+                string OtherChar = SubName[i].Substring(1);
+                SubName[i] = FirstChar.ToUpper() + OtherChar.ToLower();
+                temp += SubName[i] + " ";
+            }
+            var xoakitu = new string[] {"'", ",", "<", ".", ">", "/", "?", ";", ":", "|", "}", "]", "[", "{", "=", "+", "-", "_", ")", "(", "*", "&", "^", "%", "$", "#", "@", "!", "`", "~"};
+            foreach (var a in xoakitu)
+            {
+                temp = temp.Replace(a, string.Empty);
+            }
+
+            Console.WriteLine("Ket Qua: " + temp);
+        }
+    }
+} 
