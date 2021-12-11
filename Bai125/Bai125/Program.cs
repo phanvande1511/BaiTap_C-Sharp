3a1a547d2abbe7c0454d15b0b56accede9c1fee4
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-11 09:16:08 +0700

    BaiTap

---
+++ Bai125/Bai125/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bai125
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int max = 100;
+            int dem = 0;
+            for (int i = 2; i <= max; i++)
+            {
+                if (PrimeNumber(i) == 1)
+                {
+                    dem++;
+                }
+            }
+            Console.WriteLine("So luong so ngto nho hon 100 la: " + dem);
+        }
+
+        static int PrimeNumber(int max)
+        {
+            int temp = (int)Math.Sqrt(max);
+            for (int i = 2; i <= temp; i++)
+            {
+                if (max % i == 0)
+                {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
