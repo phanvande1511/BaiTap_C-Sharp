25da6aba4e3f72c7f5577025a8ae75995c4270f4
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-23 13:00:11 +0700

    BaiTap

---
+++ Lab9_Cau2/Lab9_Cau2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab9_Cau2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Stack listNumber = new Stack();
+            int temp;
+
+            infor();
+
+            do
+            {
+                Console.Write("\nChoose: ");
+                temp = int.Parse(Console.ReadLine());
+
+                switch(temp)
+                {
+                    case 1:
+                        for(int i = 0; i < 5; i++)
+            {
+                            listNumber.Push(new Random().Next(1, 10));
+                        }
+                        foreach (int temp1 in listNumber)
+                        {
+                            Console.Write(temp1 + " ");
+                        }
+                        break;
+
+                    case 2:
+                        listNumber.Push(new Random().Next(1, 10));
+                        foreach (int temp1 in listNumber)
+                        {
+                            Console.Write(temp1 + " ");
+                        }
+                        break;
+
+                    case 3:
+                        listNumber.Pop();
+                        foreach(int temp1 in listNumber)
+                        {
+                            Console.Write(temp1 + " ");
+                        }
+                        break;
+
+                    case 4:
+                        Console.Write(listNumber.Peek());
+                        break;
+
+                    case 5:
+                        foreach(int temp1 in listNumber)
+                        {
+                            Console.Write(temp1 + " ");
+                        }
+                        break;
+
+                    case 0:
+                        Console.Write("Thoat");
+                        break;
+                    default:
+                        Console.Write("Nhap Loi");
+                        break;
+                }
+            } while (temp >= 1 && temp <= 5);
+        }
+
+        static void infor()
+        {
+            Console.WriteLine("Demo cac thao tac tren so nguyen Java");
+            Console.WriteLine("\n=====================================\n");
+            Console.WriteLine("1. Khoi tao stack");
+            Console.WriteLine("2. Them 1 phan tu vao stack");
+            Console.WriteLine("3. Lay phan tu dau ra khoi danh sach");
+            Console.WriteLine("4. Xem phan tu dau stack");
+            Console.WriteLine("5. Duyet stack");
+            Console.WriteLine("0. Thoat");
+        }
+    }
+}
---
+++ Lab9_Cau3/Lab9_Cau3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab9_Cau3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Queue listNumber = new Queue();
+            int temp1;
+
+            infor();
+
+            do
+            {
+                Console.Write("\nChoose: ");
+                temp1 = int.Parse(Console.ReadLine());
+
+                switch (temp1)
+                {
+                    case 1:
+                        for (int i = 0; i < 5; i++)
+                        {
+                            listNumber.Enqueue(new Random().Next(1, 10));
+                        }
+                        foreach (int temp in listNumber)
+                        {
+                            Console.Write(temp + " ");
+                        }
+                        break;
+                    case 2:
+                        listNumber.Enqueue(new Random().Next(1, 10));
+                        foreach (int temp in listNumber)
+                        {
+                            Console.Write(temp + " ");
+                        }
+                        break;
+                    case 3:
+                        listNumber.Dequeue();
+                        foreach (int temp in listNumber)
+                        {
+                            Console.Write(temp + " ");
+                        }
+                        break;
+                    case 4:
+                        Console.Write(listNumber.Peek());
+                        break;
+                    case 5:
+                        foreach (int temp in listNumber)
+                        {
+                            Console.Write(temp + " ");
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("Thoat");
+                        break;
+                }
+            } while (temp1 >= 1 && temp1 <= 5);
+        }
+
+        static void infor()
+        {
+            Console.WriteLine("Demo cac thao tac tren Queue so nguyen Java");
+            Console.WriteLine("\n===========================================\n");
+            Console.WriteLine("1. Khoi tao Queue");
+            Console.WriteLine("2. Them 1 phan tu vao Queue");
+            Console.WriteLine("3. Lay Phan tu dau ra khoi Queue");
+            Console.WriteLine("4. Xem phan tu dau Queue");
+            Console.WriteLine("5. Duyet phan tu");
+            Console.WriteLine("0. Thoat");
+        }
+    }
+}
