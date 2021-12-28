90573f9efdb96f2a006604b6c911fbf19af0603a
Phan Van De <depv.12010121034@vtc.edu.vn>
2021-12-28 08:37:06 +0700

    BaiTap

---
+++ Lab9_Cau4/Lab9_Cau4/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+
+namespace Lab9_Cau4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Student student = new Student("Nguyen Van A", 01234567, 18, 7.9);
+            Student student1 = new Student("Nguyen Van B", 01234568, 19, 8);
+
+            ArrayList studentList = new ArrayList();
+
+            int temp;
+
+            infor();
+
+            do
+            {
+                Console.Write("\nChon: ");
+                temp = int.Parse(Console.ReadLine());
+
+                switch(temp)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        studentList.Add(student);
+                        student.information();
+                        break;
+                    case 2:
+                        Console.WriteLine();
+                        studentList.Add(student1);
+                        student1.information();
+                        break;
+                    case 3:
+                        foreach (Student student2 in studentList)
+                        {
+                            student2.information();
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Nhap ten Sv muon tim: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine();
+                        Student obj = null;
+
+                        foreach (Student nameCheck in studentList)
+                        {
+                            if (name == nameCheck.getName())
+                            {
+                                obj = nameCheck;
+                            }
+                        }
+
+                        if (obj != null)
+                        {
+                            obj.information();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Khong co trong danh sach");
+                        }
+
+                        break;
+                    case 5:
+                        Console.Write("Nhap ten Sv muon tim: ");
+                        string name1 = Console.ReadLine();
+                        Console.WriteLine();
+                        Student obj1 = null;
+
+                        foreach (Student nameCheck in studentList)
+                        {
+                            if (name1 == nameCheck.getName())
+                            {
+                                obj1 = nameCheck;
+                            }
+                        }
+
+                        if (obj1 != null)
+                        {
+                            Console.Write("Nhap diem muon sua: ");
+                            double newMark = float.Parse(Console.ReadLine());
+
+                            foreach (Student newMARK in studentList)
+                            {
+                                if (newMark == newMARK.getMark())
+                                {
+                                    obj1 = newMARK;
+                                }
+                            }
+
+                            if (obj1 != null)
+                            {
+                                obj1.setMark(newMark);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Khong co trong danh sach");
+                        }
+                        break;
+                    case 6:
+                        foreach (Student student3 in studentList)
+                        {
+                            student3.information();
+                            student3.test();
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 7:
+                        foreach (Student student4 in studentList)
+                        {
+                            student4.infor();
+                        }
+                        break;
+                    case 8:
+                        if(student.getMark() > student1.getMark())
+                        {
+                            student.sort();
+                            student1.sort();
+                        }
+                        if(student1.getMark() > student.getMark())
+                        {
+                            student1.sort();
+                            student.sort();
+                        }
+                        break;
+                    case 9:
+                        int temp2 = 0;
+
+                        for (int i = student.getName().Length - 1; i >= 0; i--)
+                        {
+                            if (student.getName()[i] == 32)
+                            {
+                                temp = i;
+                                break;
+                            }
+                        }
+
+                        string strg = student.getName().Substring(temp + 1);
+
+                        int temp1 = 0;
+
+                        for (int i = student1.getName().Length - 1; i >= 0; i--)
+                        {
+                            if (student1.getName()[i] == 32)
+                            {
+                                temp1 = i;
+                                break;
+                            }
+                        }
+
+                        string strg1 = student1.getName().Substring(temp1 + 1);
+
+                        if (((int)strg[0] < (int)strg1[0]))
+                        {
+                            Console.WriteLine(student.getName());
+                            Console.WriteLine(student1.getName());
+                        }
+                        else
+                        {
+                            Console.WriteLine(student1.getName());
+                            Console.WriteLine(student.getName());
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("Thoat");
+                        break;
+                    default:
+                        Console.WriteLine("Nhap sai");
+                        break;
+                }
+            } while (temp >= 1 && temp <= 9);
+        }
+
+        static void infor()
+        {
+            Console.WriteLine("Chuong trinh quan ly danh sach sinh vien su dung ArrayList");
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("1. Khoi tao danh sach");
+            Console.WriteLine("2. Them 1 sinh vien vao danh sach");
+            Console.WriteLine("3. Duyet danh sach");
+            Console.WriteLine("4. Tim thong tin sinh vien theo ten sinh vien");
+            Console.WriteLine("5. Sua diem cho sinh vien");
+            Console.WriteLine("6. Danh sach sinh vien duoc nhan hoc bong");
+            Console.WriteLine("7. Xem diem cua sinh vien");
+            Console.WriteLine("8. Sap xep danh sach giam dan theo DTB");
+            Console.WriteLine("9. Sap xep danh sach giam dan theo DTB, tang dan theo ten");
+            Console.WriteLine("0. Thoat");
+        }
+    }
+}
---
+++ Lab9_Cau4/Lab9_Cau4/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Lab9_Cau4
+{
+    public class Student
+    {
+        private int studentCode;
+        private string name;
+        private int age;
+        private double mediumPoint;
+
+        public Student(string name, int studentCode , int age, double mediumPoint)
+        {
+            this.studentCode =  studentCode;
+            this.name = name;
+            this.age = age;
+            this.mediumPoint = mediumPoint;
+        } 
+
+        public void information()
+        {
+            Console.WriteLine("Ten Sv: {0}", name);
+            Console.WriteLine("Ma Sv: {0}", studentCode);
+            Console.WriteLine("Tuoi: {0}", age);
+            Console.WriteLine("DTB: {0}", mediumPoint);
+        }
+
+        public void infor()
+        {
+            Console.WriteLine("Ten Sv: {0}", name);
+            Console.WriteLine("DTB: {0}", mediumPoint);
+        }
+
+        public void test()
+        {
+            if (mediumPoint >= 8)
+            {
+                Console.WriteLine("Du dieu kien nhan hoc bong");
+            } else
+            {
+                Console.WriteLine("Khong du dieu kien nhan hoc bong");
+            }
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public double getMark()
+        {
+            return mediumPoint;
+        }
+
+        public void setMark(double Mark)
+        {
+            this.mediumPoint = Mark;
+        }
+
+        public void sort()
+        {
+            Console.WriteLine();
+            Console.Write("Ten Sv: {0}     ", name);
+            Console.Write("DTB: {0}", mediumPoint);
+            Console.WriteLine();
+        }
+    }
+}
