0ecd3745c61a3c29125b202812ebddd32735b932
Phan Van De <depv.12010121034@vtc.edu.vn>
2022-01-13 10:41:44 +0700

    BT Node

---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LinkedList
+{
+    class Node
+    {
+        public Node next;
+        public int data;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class MainClass
+    {
+        public static Node head = null;
+
+        public static void Main(string[] args)
+        {
+            insertHead(1);
+            insertHead(2);
+            insertHead(3);
+            insertTail(9);
+            print();
+        }
+
+        static void print()
+        {
+            Node a = head;
+            if(head == null)
+            {
+                Console.WriteLine("...");
+            } else
+            {
+                Console.WriteLine(a.data);
+                while (a.next != null)
+                {
+                    a = a.next;
+                    Console.WriteLine(a.data);
+                }
+            }
+        }
+
+        static void insertHead(int n)
+        {
+            Node node = new Node(n);
+            if(head == null)
+            {
+                head = node;
+            } else
+            {
+                node.next = head;
+                head = node;
+            }
+        }
+
+        static void insertTail(int n)
+        {
+            Node node = new Node(n);
+            Node temp = null;
+
+            if(head == null)
+            {
+                head = node;
+            } else
+            {
+                temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = node;
+            }
+        }
+    }
+}
